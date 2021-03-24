bdcd56317b2600bb7903573815931f32f6961179
David Yappeter <davidyappeter@pmberjaya.com>
2021-03-24 15:27:12 +0700

    fix library dependency

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import { Form } from "semantic-ui-react";
+import { Form } from "semantic-ui-react";
 
 const Register = () => {
   const [values, setValues] = useState({
@@ -9,21 +9,47 @@ const Register = () => {
     confirmPassword: "",
   });
 
-  const onChange = () => {};
-  const onSubmit = () => {};
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prev) => ({ ...prev, [name]: value }));
+  };
+  const onSubmit = () => {
+    setValues({
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    });
+  };
 
   return (
     <div>
-      {/* <Form onSubmite={onSubmit} noValidate>
-        <h1>Register</h1>
+      <Form onSubmite={onSubmit} noValidate>
+        <h1 style={{ textAlign: "center" }}>Register</h1>
         <Form.Input
-          label="name"
+          label="Name"
           placeholder="Name.."
           name="name"
           value={values.name}
           onChange={onChange}
         />
-      </Form> */}
+        <Form.Input
+          label="Password"
+          placeholder="Password"
+          name="password"
+          value={values.password}
+          onChange={onChange}
+          type="password"
+        />
+        <Form.Input
+          label="Email"
+          placeholder="Email"
+          name="email"
+          value={values.email}
+          onChange={onChange}
+          type="email"
+        />
+      </Form>
     </div>
   );
 };

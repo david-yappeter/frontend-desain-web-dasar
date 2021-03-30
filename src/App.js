449f6d11b9525590efc6ac862bcc7d309a0091be
David Yappeter <davidyappeter@pmberjaya.com>
2021-03-30 16:00:58 +0700

    finishing up

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ import {
 } from "@apollo/client";
 import { BrowserRouter, Route } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
-import { createUploadLink } from 'apollo-upload-client'
-import { Home, Register, Login , SinglePost, Profile} from "./pages/index";
+import { createUploadLink } from "apollo-upload-client";
+import { Home, Register, Login, SinglePost, Profile } from "./pages/index";
 import { Container } from "semantic-ui-react";
 import MenuBar from "./components/MenuBar";
 
 const App = () => {
   const httpLink = createUploadLink({
     uri: process.env.REACT_APP_GRAPHQL_LINK,
-  })
+  });
 
   const authMiddleware = new ApolloLink((operation, forward) => {
     const customHeaders = operation.getContext().hasOwnProperty("headers")
